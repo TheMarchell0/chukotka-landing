d8a99bc7f43ae3d07e217ddc6be93a076de795c3
TheMarchell0 <TheMarchell0@yandex.ru>
2023-12-27 02:39:58 +0600

    add sliders

--- js/createSliders.js
+++ js/createSliders.js
@@ -8,6 +8,16 @@ function createSliders() {
             prevEl: ".activities__slider-prev-arrow",
         },
     });
+
+    const teamSlider = new Swiper(".team__slider", {
+        loop: false,
+        slidesPerView: 3,
+        spaceBetween: 75,
+        navigation: {
+            nextEl: ".team__slider-next-arrow",
+            prevEl: ".team__slider-prev-arrow",
+        },
+    });
 }
 
 export default createSliders;
