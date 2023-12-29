667d271274390628a543fef5e33606d8a40c6c87
unknown <TheMarchell0@yandex.ru>
2023-12-29 14:29:17 +0600

    add links in resources block

--- js/functions/createMobileMenu.js
+++ js/functions/createMobileMenu.js
@@ -1,15 +1,18 @@
 function createMobileMenu() {
+    const headerTop = document.querySelector('.header__top');
     const triggerButton = document.querySelector('.js-trigger-mobile-menu');
     const mobileMenu = document.querySelector('.js-mobile-menu');
     const menuItems = mobileMenu.querySelectorAll('.header__nav-list-item');
 
     triggerButton.addEventListener('click',()=> {
+        headerTop.classList.toggle('open');
         mobileMenu.classList.toggle('open');
         triggerButton.classList.toggle('open')
     })
 
     for (let menuItem of menuItems) {
         menuItem.addEventListener('click',()=> {
+            headerTop.classList.remove('open');
             mobileMenu.classList.remove('open');
             triggerButton.classList.remove('open')
         })
