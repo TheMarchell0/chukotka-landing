b087394e157b5668076529c75d179c3d2e5056de
unknown <TheMarchell0@yandex.ru>
2024-10-28 15:10:16 +0600

    add new docs in orv page

--- js/orv.js
+++ js/orv.js
@@ -8,6 +8,7 @@ import createAccordionFunctional from "./functions/createAccordionFunctional.js"
 document.addEventListener("DOMContentLoaded", function () {
     const tabsBlocks = document.querySelectorAll('.js-tabs-block');
     const accordionBlocks = document.querySelectorAll('.js-accordion');
+    const docsSliders = document.querySelectorAll('.js-orv-docs-slider');
 
     tabsBlocks.forEach(tabsBlock => createTabsFunctional(tabsBlock));
     accordionBlocks.forEach(accordionBlock => createAccordionFunctional(accordionBlock));
@@ -15,4 +16,42 @@ document.addEventListener("DOMContentLoaded", function () {
     createFormValidation();
     createAnchorsFunctional();
     createMobileMenu();
+
+    for (let docsSlider of docsSliders) {
+        const items = docsSlider.querySelectorAll('.orv-docs__list-item.swiper-slide').length;
+
+
+        if ((items > 4) || (innerWidth < 985)) {
+
+            if (!docsSlider.classList.contains('slider')) {
+                docsSlider.classList.add('slider');
+            }
+
+            const newsSlider = new Swiper(docsSlider, {
+                loop: false,
+                navigation: {
+                    nextEl: docsSlider.querySelector('.orv-docs__next-arrow'),
+                    prevEl: docsSlider.querySelector('.orv-docs__prev-arrow'),
+                },
+                spaceBetween: 20,
+                breakpoints: {
+                    320: {
+                        slidesPerView: 1.5,
+                    },
+                    600: {
+                        slidesPerView: 2,
+                    },
+                    985: {
+                        slidesPerView: 3,
+                    },
+                    1024: {
+                        width: 310,
+                    },
+                    1400: {
+                        slidesPerView: 4,
+                    }
+                }
+            });
+        }
+    }
 });
