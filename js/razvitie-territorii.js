004b897d9a9d27521e4daac653456d4d79d32152
TheMarchell0 <TheMarchell0@yandex.ru>
2025-04-14 16:53:46 +0600

    add desktop razvitie-territorii page

--- js/razvitie-territorii.js
+++ js/razvitie-territorii.js
@@ -2,16 +2,10 @@ import createModalFunctional from "./functions/createModalFunctional.js";
 import createFormValidation from "./functions/createFormValidation.js";
 import createMobileMenu from "./functions/createMobileMenu.js";
 import {createCookieFunctional} from "./functions/createCookieFunctional.js";
-import createTabsFunctional from "./functions/createTabsFunctional.js";
-import createAccordionFunctional from "./functions/createAccordionFunctional.js";
+import createFAQBlockFunctional from "./functions/createFAQBlockFunctional.js";
 
 document.addEventListener("DOMContentLoaded", function () {
-    const tabsBlocks = document.querySelectorAll('.js-tabs-block');
-    const accordionBlocks = document.querySelectorAll('.js-accordion');
-
-    tabsBlocks.forEach(tabsBlock => createTabsFunctional(tabsBlock));
-    accordionBlocks.forEach(accordionBlock => createAccordionFunctional(accordionBlock));
-
+    createFAQBlockFunctional();
     createCookieFunctional();
     createModalFunctional();
     createFormValidation();
