1b26f32acc56bc5605664c003ddd6de648357239
TheMarchell0 <TheMarchell0@yandex.ru>
2025-04-17 14:28:39 +0600

    finish privlechenie-investiciy page (desktop/adaptive)

--- js/privlechenie-investiciy.js
+++ js/privlechenie-investiciy.js
@@ -2,8 +2,15 @@ import createModalFunctional from "./functions/createModalFunctional.js";
 import createFormValidation from "./functions/createFormValidation.js";
 import createMobileMenu from "./functions/createMobileMenu.js";
 import {createCookieFunctional} from "./functions/createCookieFunctional.js";
+import createFAQBlockFunctional from "./functions/createFAQBlockFunctional.js";
+import createAccordionFunctional from "./functions/createAccordionFunctional.js";
 
 document.addEventListener("DOMContentLoaded", function () {
+    const accordionBlocks = document.querySelectorAll('.js-accordion');
+
+    accordionBlocks.forEach(accordionBlock => createAccordionFunctional(accordionBlock));
+
+    createFAQBlockFunctional();
     createCookieFunctional();
     createModalFunctional();
     createFormValidation();
