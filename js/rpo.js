080a32241610f0885fe31e1ba2e6264c4713a921
TheMarchell0 <TheMarchell0@yandex.ru>
2025-04-29 14:33:38 +0600

    start rpo page

---
+++ js/rpo.js
@@ -0,0 +1,23 @@
+import createModalFunctional from "./functions/createModalFunctional.js";
+import createFormValidation from "./functions/createFormValidation.js";
+import createMobileMenu from "./functions/createMobileMenu.js";
+import {createCookieFunctional} from "./functions/createCookieFunctional.js";
+import createFAQBlockFunctional from "./functions/createFAQBlockFunctional.js";
+
+document.addEventListener("DOMContentLoaded", function () {
+    createFAQBlockFunctional();
+    createCookieFunctional();
+    createModalFunctional();
+    createFormValidation();
+    createMobileMenu();
+
+    const projectsSlider = new Swiper(".projects__slider", {
+        loop: false,
+        slidesPerView: 1,
+        navigation: {
+            nextEl: ".projects__slider-next-arrow",
+            prevEl: ".projects__slider-prev-arrow",
+        },
+        spaceBetween: 20,
+    });
+});
