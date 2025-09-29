61ce77845e61fd81cb0c5e35f2e326508c582dc1
unknown <TheMarchell0@yandex.ru>
2025-09-29 19:27:12 +0600

    change orv page structure

--- js/orv.js
+++ js/orv.js
@@ -2,11 +2,14 @@ import createModalFunctional from "./functions/createModalFunctional.js";
 import createFormValidation from "./functions/createFormValidation.js";
 import createAnchorsFunctional from "./functions/createAnchorsFunctional.js";
 import createMobileMenu from "./functions/createMobileMenu.js";
+import createAccordionFunctional from "./functions/createAccordionFunctional.js";
 import {createCookieFunctional} from "./functions/createCookieFunctional.js";
 import createFAQBlockFunctional from "./functions/createFAQBlockFunctional.js";
 
 document.addEventListener("DOMContentLoaded", function () {
-    const docsSliders = document.querySelectorAll('.js-orv-docs-slider');
+    const accordionBlocks = document.querySelectorAll('.js-accordion');
+
+    accordionBlocks.forEach(accordionBlock => createAccordionFunctional(accordionBlock));
 
     createFAQBlockFunctional();
     createCookieFunctional();
@@ -14,42 +17,4 @@ document.addEventListener("DOMContentLoaded", function () {
     createFormValidation();
     createAnchorsFunctional();
     createMobileMenu();
-
-    for (let docsSlider of docsSliders) {
-        const items = docsSlider.querySelectorAll('.orv-docs__list-item.swiper-slide').length;
-
-
-        if ((items > 4) || (innerWidth < 1320)) {
-
-            if (!docsSlider.classList.contains('slider')) {
-                docsSlider.classList.add('slider');
-            }
-
-            const newsSlider = new Swiper(docsSlider, {
-                loop: false,
-                navigation: {
-                    nextEl: docsSlider.querySelector('.orv-docs__next-arrow'),
-                    prevEl: docsSlider.querySelector('.orv-docs__prev-arrow'),
-                },
-                spaceBetween: 20,
-                breakpoints: {
-                    320: {
-                        slidesPerView: 1.5,
-                    },
-                    600: {
-                        slidesPerView: 2,
-                    },
-                    985: {
-                        slidesPerView: 3,
-                    },
-                    1024: {
-                        width: 310,
-                    },
-                    1400: {
-                        slidesPerView: 4,
-                    }
-                }
-            });
-        }
-    }
 });
